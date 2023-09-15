df256ec7d7838cfbae73bddc3658a6d5f1c3b8e6
I.P <72142537+IliyaYavorovPetrov@users.noreply.github.com>
2023-09-15 15:49:30 +0300

    extend the models

--- app/routing/reqroutinginfo.go
+++ app/routing/reqroutinginfo.go
@@ -1,10 +1,10 @@
 package routing
 
 type ReqRoutingInfo struct {
-	SourceURL      string `redis:"sourceURL"`
-	DestinationURL string `redis:"destinationURL"`
-	MethodHTTP     string `redis:"methodHTTP"`
-	IsAuthNeeded   bool   `redis:"isAuthNeeded"`
+	SourceURL      string `redis:"sourceURL" json:"sourceURL"`
+	DestinationURL string `redis:"destinationURL" json:"destinationURL"`
+	MethodHTTP     string `redis:"methodHTTP" json:"methodHTTP"`
+	IsAuthNeeded   bool   `redis:"isAuthNeeded" json:"isAuthNeeded"`
 }
 
 func NewReqRoutingInfo(sourceURL string, destinationURL string, methodHTTP string, isAuthNeeded bool) (ReqRoutingInfo, error) {
