78c0230e0f27752ae59f527cedc88dbf06eac221
IliyaYavorovPetrov <ilia.yavorov.petrov@gmail.com>
2023-09-20 07:47:18 +0300

    todos

---
+++ app/gateways/cache/cache.go
@@ -0,0 +1,6 @@
+package cache
+
+// TODO: here will refresh the distributed cache
+// TODO: in the interface should have 2 methods
+//			- for creating a instance of a cahce (private)
+//			- for accessing the instance (public)
