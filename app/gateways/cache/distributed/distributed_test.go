76605139eff5099396ad6f498bce9e9e9e7847e1
IliyaYavorovPetrov <ilia.yavorov.petrov@gmail.com>
2023-09-24 21:41:18 +0300

    tmp update test distributed

--- app/gateways/cache/distributed/distributed_test.go
+++ app/gateways/cache/distributed/distributed_test.go
@@ -1,6 +1,7 @@
 package distributed_test
 
 import (
+	"context"
 	"github.com/IliyaYavorovPetrov/api-gateway/app/gateways/cache/distributed"
 	"github.com/IliyaYavorovPetrov/api-gateway/test"
 	"testing"
@@ -8,7 +9,7 @@ import (
 
 func TestAddAndGet(t *testing.T) {
 	test.ClearDistributedCache()
-	gw := distributed.GetInstance()
+	cache := distributed.GetInstance()
 
 	key := "key1"
 	val := test.ItemTest{
@@ -17,12 +18,12 @@ func TestAddAndGet(t *testing.T) {
 		Size:    45.67,
 	}
 
-	err := gw.Add(test.GetCtx(), key, val)
+	err := cache.Add(test.GetCtx(), key, val)
 	if err != nil {
 		t.Errorf("Add failed: %v", err)
 	}
 
-	res, err := gw.Get(test.GetCtx(), key)
+	res, err := cache.Get(test.GetCtx(), key)
 	if err != nil {
 		t.Errorf("Get failed: %v", err)
 	}
@@ -31,143 +32,143 @@ func TestAddAndGet(t *testing.T) {
 	}
 }
 
-//func TestAddAllItems(t *testing.T) {
-//	gw := distributed.GetInstance()
-//	test.ClearDistributedCache()
-//
-//	data := map[string]interface{}{
-//		"key1": test.ItemTest{
-//			ID:      1,
-//			Content: "content 1",
-//			Size:    45.67,
-//		},
-//		"key2": test.ItemTest{
-//			ID:      2,
-//			Content: "content 2",
-//			Size:    23.02,
-//		},
-//	}
-//
-//	err := gw.AddAllItems(context.Background(), data)
-//	if err != nil {
-//		t.Errorf("AddAllItems failed: %v", err)
-//	}
-//
-//	for key, val := range data {
-//		res, err := gw.Get(test.GetCtx(), key)
-//		if err != nil {
-//			t.Errorf("Get failed: %v", err)
-//		}
-//		if res != val {
-//			t.Errorf("expected %s, but got %s", val, res)
-//		}
-//	}
-//}
-//
-//func TestGetAllKeysByPrefix(t *testing.T) {
-//	gw := distributed.GetInstance()
-//	test.ClearDistributedCache()
-//
-//	data := map[string]interface{}{
-//		"test:key:key1": test.ItemTest{
-//			ID:      1,
-//			Content: "content 1",
-//			Size:    45.67,
-//		},
-//		"test:key:key2": test.ItemTest{
-//			ID:      2,
-//			Content: "content 2",
-//			Size:    23.02,
-//		},
-//		"wrong:key:key2": test.ItemTest{
-//			ID:      3,
-//			Content: "content 3",
-//			Size:    173.87,
-//		},
-//	}
-//
-//	err := gw.AddAllItems(test.GetCtx(), data)
-//	if err != nil {
-//		t.Errorf("AddAllItems failed: %v", err)
-//	}
-//
-//	prefix := "test:key:"
-//	keys, err := gw.GetAllKeysByPrefix(test.GetCtx(), prefix)
-//	if err != nil {
-//		t.Errorf("GetAllKeysByPrefix failed: %v", err)
-//	}
-//
-//	exp := []string{"test:key:key1", "test:key:key2"}
-//	for _, key := range exp {
-//		if !test.ContainsItem(key, keys) {
-//			t.Errorf("expected to contain %v, but it did not", key)
-//		}
-//	}
-//}
-//
-//func TestGetAllItems(t *testing.T) {
-//	gw := distributed.GetInstance()
-//	test.ClearDistributedCache()
-//
-//	data := map[string]interface{}{
-//		"test:key:key1": test.ItemTest{
-//			ID:      1,
-//			Content: "content 1",
-//			Size:    45.67,
-//		},
-//		"test:key:key2": test.ItemTest{
-//			ID:      2,
-//			Content: "content 2",
-//			Size:    23.02,
-//		},
-//	}
-//
-//	err := gw.AddAllItems(test.GetCtx(), data)
-//	if err != nil {
-//		t.Errorf("AddAllItems failed: %v", err)
-//	}
-//
-//	items, err := gw.GetAllItems(test.GetCtx())
-//	if err != nil {
-//		t.Errorf("GetAllItems failed: %v", err)
-//	}
-//
-//	if !test.MapEqual(items, data) {
-//		t.Errorf("expected %v, but got %v", data, items)
-//	}
-//}
-//
-//func TestDelete(t *testing.T) {
-//	gw := distributed.GetInstance()
-//	test.ClearDistributedCache()
-//
-//	data := map[string]interface{}{
-//		"test:key:key1": test.ItemTest{
-//			ID:      1,
-//			Content: "content 1",
-//			Size:    45.67,
-//		},
-//		"test:key:key2": test.ItemTest{
-//			ID:      2,
-//			Content: "content 2",
-//			Size:    23.02,
-//		},
-//	}
-//
-//	delKey := "test:key:key1"
-//
-//	err := gw.AddAllItems(test.GetCtx(), data)
-//	if err != nil {
-//		t.Errorf("AddAllItems failed: %v", err)
-//	}
-//
-//	err = gw.Delete(test.GetCtx(), delKey)
-//	if err != nil {
-//		t.Errorf("Delete failed: %v", err)
-//	}
-//
-//	_, err = gw.Get(test.GetCtx(), delKey)
-//	if err == nil {
-//		t.Errorf("expected error for Get after deletion, but got nil")
-//	}
-//}
+func TestAddAllItems(t *testing.T) {
+	test.ClearDistributedCache()
+	cache := distributed.GetInstance()
+
+	data := map[string]interface{}{
+		"key1": test.ItemTest{
+			ID:      1,
+			Content: "content 1",
+			Size:    45.67,
+		},
+		"key2": test.ItemTest{
+			ID:      2,
+			Content: "content 2",
+			Size:    23.02,
+		},
+	}
+
+	err := cache.AddAllItems(context.Background(), data)
+	if err != nil {
+		t.Errorf("AddAllItems failed: %v", err)
+	}
+
+	for key, val := range data {
+		res, err := cache.Get(test.GetCtx(), key)
+		if err != nil {
+			t.Errorf("Get failed: %v", err)
+		}
+		if res != val {
+			t.Errorf("expected %s, but got %s", val, res)
+		}
+	}
+}
+
+func TestGetAllKeysByPrefix(t *testing.T) {
+	test.ClearDistributedCache()
+	cache := distributed.GetInstance()
+
+	data := map[string]interface{}{
+		"test:key:key1": test.ItemTest{
+			ID:      1,
+			Content: "content 1",
+			Size:    45.67,
+		},
+		"test:key:key2": test.ItemTest{
+			ID:      2,
+			Content: "content 2",
+			Size:    23.02,
+		},
+		"wrong:key:key2": test.ItemTest{
+			ID:      3,
+			Content: "content 3",
+			Size:    173.87,
+		},
+	}
+
+	err := cache.AddAllItems(test.GetCtx(), data)
+	if err != nil {
+		t.Errorf("AddAllItems failed: %v", err)
+	}
+
+	prefix := "test:key:"
+	keys, err := cache.GetAllKeysByPrefix(test.GetCtx(), prefix)
+	if err != nil {
+		t.Errorf("GetAllKeysByPrefix failed: %v", err)
+	}
+
+	exp := []string{"test:key:key1", "test:key:key2"}
+	for _, key := range exp {
+		if !test.ContainsItem(key, keys) {
+			t.Errorf("expected to contain %v, but it did not", key)
+		}
+	}
+}
+
+func TestGetAllItems(t *testing.T) {
+	test.ClearDistributedCache()
+	cache := distributed.GetInstance()
+
+	data := map[string]interface{}{
+		"test:key:key1": test.ItemTest{
+			ID:      1,
+			Content: "content 1",
+			Size:    45.67,
+		},
+		"test:key:key2": test.ItemTest{
+			ID:      2,
+			Content: "content 2",
+			Size:    23.02,
+		},
+	}
+
+	err := cache.AddAllItems(test.GetCtx(), data)
+	if err != nil {
+		t.Errorf("AddAllItems failed: %v", err)
+	}
+
+	items, err := cache.GetAllItems(test.GetCtx())
+	if err != nil {
+		t.Errorf("GetAllItems failed: %v", err)
+	}
+
+	if !test.MapEqual(items, data) {
+		t.Errorf("expected %v, but got %v", data, items)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	test.ClearDistributedCache()
+	cache := distributed.GetInstance()
+
+	data := map[string]interface{}{
+		"test:key:key1": test.ItemTest{
+			ID:      1,
+			Content: "content 1",
+			Size:    45.67,
+		},
+		"test:key:key2": test.ItemTest{
+			ID:      2,
+			Content: "content 2",
+			Size:    23.02,
+		},
+	}
+
+	delKey := "test:key:key1"
+
+	err := cache.AddAllItems(test.GetCtx(), data)
+	if err != nil {
+		t.Errorf("AddAllItems failed: %v", err)
+	}
+
+	err = cache.Delete(test.GetCtx(), delKey)
+	if err != nil {
+		t.Errorf("Delete failed: %v", err)
+	}
+
+	_, err = cache.Get(test.GetCtx(), delKey)
+	if err == nil {
+		t.Errorf("expected error for Get after deletion, but got nil")
+	}
+}
