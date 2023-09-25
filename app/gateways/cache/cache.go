d15e7c46f71885a0a013be7624d7f28a9aaf8a80
I.P <72142537+IliyaYavorovPetrov@users.noreply.github.com>
2023-09-25 17:20:35 +0300

    tmp sync mech for caches

--- app/gateways/cache/cache.go
+++ app/gateways/cache/cache.go
@@ -1,1 +1,36 @@
 package cache
+
+import (
+	"context"
+	"github.com/IliyaYavorovPetrov/api-gateway/app/gateways"
+)
+
+// If distributed write fails, we have inconsistent state of cache
+
+func WriteToLocalAndDistributedCaches(ctx context.Context, local gateways.Cache, distributed gateways.Cache, key string, data interface{}) error {
+	err := local.Add(ctx, key, data)
+	if err != nil {
+		return err
+	}
+
+	err = distributed.Add(ctx, key, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Again can have inconsistent state of cache c1
+
+func LoadCacheOneWithCacheTwo(ctx context.Context, c1 gateways.Cache, c2 gateways.Cache) error {
+	items, err := c2.GetAllItems(ctx)
+	if err != nil {
+		return err
+	}
+	err = c1.AddAllItems(ctx, items)
+	if err != nil {
+		return err
+	}
+	return nil
+}
